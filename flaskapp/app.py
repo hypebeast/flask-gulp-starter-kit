bad45de065db3cf1dfccccb73dda90089d83d27e
Sebastian Ruml <sebastian.ruml@gmail.com>
2016-11-15 21:48:46 +0100

    remove comment

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """The app module, containing the app factory function."""
-from __future__ import print_function  # (at top of module)
+from __future__ import print_function
 
 import logging
 import sys
